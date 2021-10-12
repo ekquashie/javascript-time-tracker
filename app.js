6a124f74581a2bbbdab287e45af1ba8ce8e1c59c
Eyram Quashie <eyramkobla.quashie@gmail.com>
2021-10-12 13:24:54 +0000

    json data

--- app.js
+++ app.js
@@ -1,111 +1,104 @@
 // Get JSON data
-let eventJSON = [
-  {
-    "title": "Work",
-    "timeframes": {
-      "daily": {
-        "current": 5,
-        "previous": 7
-      },
-      "weekly": {
-        "current": 32,
-        "previous": 36
-      },
-      "monthly": {
-        "current": 103,
-        "previous": 128
-      }
-    }
-  },
-  {
-    "title": "Play",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 2
-      },
-      "weekly": {
-        "current": 10,
-        "previous": 8
-      },
-      "monthly": {
-        "current": 23,
-        "previous": 29
-      }
-    }
-  },
-  {
-    "title": "Study",
-    "timeframes": {
-      "daily": {
-        "current": 0,
-        "previous": 1
-      },
-      "weekly": {
-        "current": 4,
-        "previous": 7
-      },
-      "monthly": {
-        "current": 13,
-        "previous": 19
-      }
-    }
-  },
-  {
-    "title": "Exercise",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 1
-      },
-      "weekly": {
-        "current": 4,
-        "previous": 5
-      },
-      "monthly": {
-        "current": 11,
-        "previous": 18
-      }
-    }
-  },
-  {
-    "title": "Social",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 3
-      },
-      "weekly": {
-        "current": 5,
-        "previous": 10
-      },
-      "monthly": {
-        "current": 21,
-        "previous": 23
-      }
-    }
-  },
-  {
-    "title": "Self Care",
-    "timeframes": {
-      "daily": {
-        "current": 0,
-        "previous": 1
-      },
-      "weekly": {
-        "current": 2,
-        "previous": 2
-      },
-      "monthly": {
-        "current": 7,
-        "previous": 11
-      }
-    }
-  }
-];
-
-console.log(eventJSON);
+var eventData;
+const url = "./data.json"; 
 
 // Declaring Variables
+const dailyLink = document.getElementById("daily");
+const weeklyLink = document.getElementById("weekly");
+const monthlyLink = document.getElementById("monthly");
+const rightDiv = document.getElementById("rightdiv");
 
+// Event Listeners
+dailyLink.addEventListener('click', function(){
+  showData("daily");
+});
+weeklyLink.addEventListener('click', function(){
+  showData("weekly");
+});
+monthlyLink.addEventListener('click', function(){
+  showData("monthly");
+});
+
+function showData(curr) {
+  // Daily Weekly Monthly
+  location.reload();
+
+  return newFunc(curr);
+}
+
+async function newFunc(curr) {
+  var currEvent;
+  await fetch(url).then(res => {
+    return res.json();
+  }).then(data => {
+    console.log(currEvent);
+    console.log(data);
+    data.forEach(item => {
+
+      // Main routine div
+      const routine = document. createElement("div");
+      routine.setAttribute("class", "routine");
+      routine.setAttribute("id", item.title.toLowerCase());
+      
+      // Back Div with icon
+      const backDiv = document.createElement("div");
+      backDiv.setAttribute("class", "back " + item.title.toLowerCase());
+      // Actual icon
+      const backIcon = document.createElement("object");
+
+      if(item.title.toLowerCase() != "self care") {
+        backIcon.setAttribute("data", "images/icon-" + item.title.toLowerCase() + ".svg");
+      } else {
+        backIcon.setAttribute("data", "images/icon-self-care.svg");
+      }
+
+      backDiv.appendChild(backIcon);
+      routine.appendChild(backDiv);
+
+      // Front Div
+      const frontDiv = document.createElement("div");
+      frontDiv.setAttribute("class", "front");
+
+      // Main heading Div
+      const headingDiv = document.createElement("div");
+      headingDiv.setAttribute("class", "heading");
+
+      // Actual H4 Heading
+      const heading = document.createElement("h4");
+      heading.innerHTML = item.title;
+
+      // Object with Ellipsis Icon
+      const menuIcon = document.createElement("object");
+      menuIcon.setAttribute("data", "images/icon-ellipsis.svg");
+
+      headingDiv.appendChild(heading);
+      headingDiv.appendChild(menuIcon);
+
+      // Append Heading Div
+      frontDiv.appendChild(headingDiv);
+
+      // Time div
+      const timeDiv = document.createElement("div");
+      timeDiv.setAttribute("class", "time");
+
+      // Current Time
+      const currentTime = document.createElement("h1");
+      currEvent = curr;
+      currentTime.innerHTML = item.timeframes[currEvent].current + "hrs";
+
+      // Previous Time
+      const previousTime = document.createElement("p");
+      previousTime.innerHTML = "Last week - " + item.timeframes[currEvent].previous +"hrs";
+
+      timeDiv.appendChild(currentTime);
+      timeDiv.appendChild(previousTime);
+      frontDiv.appendChild(timeDiv);
+
+      routine.appendChild(frontDiv);
+
+      rightDiv.appendChild(routine);
+      // console.log(item.timeframes.monthly.current);
+    });
+  });
+}
 
